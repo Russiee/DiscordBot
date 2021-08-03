c8e11c9993fef1402425c7877245dde181abf043
unknown <nikitavorontsov@hotmail.com>
2021-08-03 08:01:48 +0100

    Resetting for video

--- src/main/java/com/example/demo/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services;
-
-import com.example.demo.entities.User;
-import com.example.demo.exceptions.UserExistsException;
-
-public interface UserService {
-
-    User createUser(String id, String name) throws UserExistsException;
-}
